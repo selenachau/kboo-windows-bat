fccceb23a1c7466319abbf0a57e70ec4239471e3
selenachau <selenachau@gmail.com>
2016-11-15 09:37:32 -0800

    update

--- wav-to-mp3.bat
+++ wav-to-mp3.bat
@@ -1,6 +1,6 @@
-for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -vn -c:a libmp3lame -r:a 44100 -b:a 192k "%%~nA.mp3"
+for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -map_metadata 0 -c:a libmp3lame -r:a 44100 -b:a 192k "%%~nA.mp3"
 
-::Nov. 15, 2016 note, SC--ffmpeg was installed on Zale's computer on the C:\ drive at the path named above. Better to install it in program files folder, then will need to adjust this script based on where ffmpeg is located, per computer running the script.
+:: Nov. 15, 2016 note, SC--ffmpeg was installed on Zale's computer on the C:\ drive at the path named above. Better to install it in program files folder, then will need to adjust this script based on where ffmpeg is located, per computer running the script.
 :: batch script for Windows 7 at KBOO
 :: keeps same filename of wav for mp3
 :: -vn disable video i.e. video no
@@ -9,3 +9,4 @@ for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -vn -c:a libmp3la
 :: libmp3lame external encoding library within ffmpeg
 :: -b:a 192k audio bitrate 
 :: full documentation at ffmpeg.org/ffmpeg.html
+:: for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -map_metadata 0 -c:a libmp3lame -r:a 44100 -b:a 192k "%%~nA.mp3"

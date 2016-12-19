f1ea291d3c76572e7235f9c8406fae9a62395bba
selenachau <selenachau@gmail.com>
2016-12-19 09:11:00 -0800

    Update wav-to-mp3.bat

--- wav-to-mp3.bat
+++ wav-to-mp3.bat
@@ -1,6 +1,6 @@
 for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -map_metadata 0 -c:a libmp3lame -r:a 44100 -q:a 3 -n "%%~nA.mp3"
 
-:: Nov. 15, 2016 note, SC--ffmpeg was installed on Zale's computer on the C:\ drive at the path named above. Better to install it in program files folder, then will need to adjust this script based on where ffmpeg is located, per computer running the script.
+:: Nov. 15, 2016 note, SC: ffmpeg was installed on Zale's computer on the C:\ drive at the path named above. Adjust this script based on where ffmpeg is located, per computer running the script.
 :: batch script for Windows 7 at KBOO
 :: keeps same filename of wav for mp3
 :: Ctrl-C to abort the script, will ask "Terminiate batch job (Y/N)?" hit N to exit the batch script

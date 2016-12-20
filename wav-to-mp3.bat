4bf58d9e8908a4915283242f64719b8cbf64d07b
selenachau <selenachau@gmail.com>
2016-12-20 11:07:24 -0800

    Update wav-to-mp3.bat

--- wav-to-mp3.bat
+++ wav-to-mp3.bat
@@ -1,4 +1,4 @@
-for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -map_metadata 0 -c:a libmp3lame -r:a 44100 -q:a 3 -n "%%~nA.mp3"
+for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -map_metadata 0 -c:a libmp3lame -r:a 44100 -q:a 1 -n "%%~nA.mp3"
 
 :: Nov. 15, 2016 note, SC: ffmpeg was installed on Zale's computer on the C:\ drive at the path named above. Adjust this script based on where ffmpeg is located, per computer running the script.
 :: batch script for Windows 7 at KBOO
@@ -9,6 +9,6 @@ for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -i
 :: -r:a or -ar 44100 sample rate
 :: -c:a or -ac  set # of audio channels. For output streams it is set by default to # input audio streams
 :: libmp3lame external encoding library within ffmpeg
-:: -q:a 3 transcode at constant quality as opposed to constant bitrate 3 is 150-195 bitrate range kbit/s
+:: -q:a 1 transcode at constant quality as opposed to constant bitrate 3 is 190-250 bitrate range kbit/s
 :: -n skips wav files where mp3 file exists with same name
 :: full documentation at ffmpeg.org/ffmpeg.html

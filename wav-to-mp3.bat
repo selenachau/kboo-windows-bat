58676d5cc15d1b78c9809c1f34bdb84c7887e6f0
selenachau <selenachau@gmail.com>
2017-01-05 12:03:52 -0800

    update reflects new ffmpeg location
    Installed ffmpeg on A:\ drive and updated notes.

--- wav-to-mp3.bat
+++ wav-to-mp3.bat
@@ -1,15 +1,14 @@
-for %%A IN (*.wav) do c:\ffmpeg\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -map_metadata 0 -c:a libmp3lame -r:a 44100 -q:a 1 -n "%%~nA.mp3"
+for %%A IN (*.wav) do A:\ffmpeg\bin\ffmpeg.exe -i "%%A" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -map_metadata 0 -c:a libmp3lame -r:a 44100 -q:a 1 -n "%%~nA.mp3"
 
-:: Dec 2016 note: ffmpeg was installed on Zale, Erin, and archive workstation computers on the C:\ drive at the path named above. Adjust this script based on where ffmpeg is located, per computer running the script.
+:: Jan 2017: ffmpeg installed on network A drive.
 :: batch script for Windows 7 at KBOO
 :: keeps same filename of wav for mp3
 :: Ctrl-C to abort the script, will ask "Terminiate batch job (Y/N)?" hit N to exit the batch script
-:: The batch script runs a loop for each wav file. To stop the loop command for an individual file, press Q to stop. This will leave you with an unfinished mp3 file and the script will move on to the next wav file queued in the loop.
+:: The batch script runs a loop for each wav file in the same folder that the script is saved in. To stop the loop command for an individual file, press Q to stop. This will leave you with an unfinished mp3 file and the script will move on to the next wav file queued in the loop.
 :: -vn disable video i.e. video no
 :: -r:a or -ar 44100 sample rate
 :: -c:a or -ac  set # of audio channels. For output streams it is set by default to # input audio streams
 :: libmp3lame external encoding library within ffmpeg
-:: -q:a 1 transcode at constant quality as opposed to constant bitrate 1 is 190-250 bitrate range kbit/s
+:: -q:a 3 transcode at constant quality as opposed to constant bitrate 3 is 19-250 bitrate range kbit/s
 :: -n skips wav files where mp3 file exists with same name
 :: full documentation at ffmpeg.org/ffmpeg.html
-:: For more detailed discussion on variable vs constant bitrates see https://trac.ffmpeg.org/wiki/Encode/MP3
